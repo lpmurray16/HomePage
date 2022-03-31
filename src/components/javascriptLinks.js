881d6872d8e3a80ca6e0d7898ce45d86fb6e23d3
Logan Murray <lpmurray16@gmail.com>
2022-03-31 16:48:45 -0500

    new set of links

---
+++ src/components/javascriptLinks.js
@@ -0,0 +1,20 @@
+const arrayOfLinks = [
+    {
+        title: "Alpine Framework",
+        link: "http://alpinejs.dev",
+    },
+    {
+        title: "React Framework",
+        link: "https://reactjs.org/",
+    },
+    {
+        title: "Svelte Framework",
+        link: "https://svelte.dev/",
+    },
+];
+
+const jsArray = {
+    headerTitle: "JS Stuff",
+    allLinksArray: arrayOfLinks,
+};
+export default jsArray;
