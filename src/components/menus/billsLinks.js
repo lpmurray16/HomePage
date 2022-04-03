d4c099422ca4344f8f93e96b7b5e3b5120a9e62f
Logan Murray <lpmurray16@users.noreply.github.com>
2022-04-03 11:00:36 -0500

    got rid of some components. menu is now alpine

---
+++ src/components/menus/billsLinks.js
@@ -0,0 +1,36 @@
+const arrayOfLinks = [
+    {
+        title: "Nicor",
+        link: "https://customerportal.southerncompany.com/User/Login?LDC=7",
+    },
+    {
+        title: "Rent",
+        link: "https://ascendstcharles.activebuilding.com/portal/payments/external?utm_source=Payments&utm_medium=email&utm_campaign=payments-email&utm_content=No-portal-account-electronic-payment",
+    },
+    {
+        title: "Water/⚡/Sewage",
+        link: "https://ipn2.paymentus.com/cp/my-accounts.action?CSRF_TOKEN=1645654868396-060234e9c7efafa912bcfe129f8099afc7f3b0ffc06b9ce64233fdb67ce6b4de",
+    },
+    {
+        title: "Amazon Card",
+        link: "https://www.synchronycredit.com/eSecurity/Login/login.action?clientId=amazon&accountType=plcc&langId=en",
+    },
+    {
+        title: "Internet",
+        link: "https://www.login.xfinity.com/",
+    },
+    {
+        title: "Upstart Loan",
+        link: "https://finwise.upstart.com/funded_borrower_dashboard",
+    },
+    {
+        title: "Car Payment",
+        link: "https://www.wellsfargo.com/",
+    },
+];
+
+const billsArray = {
+    headerTitle: "Bill$",
+    allLinksArray: arrayOfLinks,
+};
+export default billsArray;
