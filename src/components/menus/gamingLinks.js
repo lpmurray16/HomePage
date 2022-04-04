b54411f039973ed6ade67880a7333ee9078fb4b1
Logan Murray <lpmurray16@users.noreply.github.com>
2022-04-03 19:23:01 -0500

    new menu

---
+++ src/components/menus/gamingLinks.js
@@ -0,0 +1,36 @@
+const arrayOfLinks = [
+    {
+        title: "DLCompare",
+        link: "https://dlcompare.com/",
+    },
+    {
+        title: "Nexus Mods",
+        link: "https://www.nexusmods.com/",
+    },
+    {
+        title: "IGN",
+        link: "https://www.ign.com/",
+    },
+    {
+        title: "1337x",
+        link: "https://1337x.to/",
+    },
+    {
+        title: "Placeholder",
+        link: "https://www.login.xfinity.com/",
+    },
+    {
+        title: "Placeholder",
+        link: "https://finwise.upstart.com/funded_borrower_dashboard",
+    },
+    {
+        title: "Placeholder",
+        link: "https://www.wellsfargo.com/",
+    },
+];
+
+const gamesArray = {
+    headerTitle: "Gaming",
+    allLinksArray: arrayOfLinks,
+};
+export default gamesArray;
